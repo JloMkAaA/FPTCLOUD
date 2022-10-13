a452c071ff65a0cfbab22390ec50cc9799661be2
magistorS <96631431+magistorS@users.noreply.github.com>
2022-10-13 08:56:46 +0300

    selected removed index login

--- yii/web/js/main.js
+++ yii/web/js/main.js
@@ -13,13 +13,27 @@ function bg_mun(){
 
 
     function a()
-    {document.getElementById('info').innerHTML = '../uploads/sun.png' + 'drftghjk';}
+    {document.getElementById('info').innerHTML = '' + 'drftghjk';}
     function b()
     {document.getElementById('info').innerHTML = 'kod6';}   
 
-    document.getElementById('sun_btn').addEventListener('click', function() {
+    document.getElementById('sun_btn').addEventListener('click', function(e) {
   let l = document.createElement('img');
-  l.src = '../uploads/sun.png';
-  
-  document.body.appendChild(l);
+  l.classList.add("img_login");
+//   l.style.width='250px';
+
+
+
+
+    let flag = false;
+  let elemnt1 = document.getElementById('unique');
+  elemnt1.innerHTML = '<img src="../uploads/sun.png">';
+  element1.addEventListener('click', function() {
+    elemnt1.innerHTML= (flag=!flag)?'787576756':'545656';
+
+  });
+ 
+// document.body.appendChild(l);
+ // e.stopPropagation();
+ //document.body.innerHTML = '123456';s
 });  
