13719f87ccb4d76e28effa08bb0693c6dbb78000
magistorS <96631431+magistorS@users.noreply.github.com>
2022-10-17 08:43:36 +0300

    регистрация и авторизация

--- views/user/signup.php
+++ views/user/signup.php
@@ -6,19 +6,25 @@ use yii\bootstrap5\ActiveForm;
 $this->title = 'Регистрация';
 ?>
 
-<div class="container">
+<div class="container" >
 	<div class="row">
 		<div class="col-md-8 offset-md-2">
-			<div class="border rounded my-3 p-3 bg-light">
-				<h1><?= Html::encode($this->title) ?></h1>
 
-				<?php $form = ActiveForm::begin(); ?>
-				<?= $form->field($model, 'login')->textInput(['autofocus' => true]) ?>
-				<?= $form->field($model, 'password')->passwordInput() ?>
-				<?= $form->field($model, 'name')->textInput() ?>
-				<div class="form-group"><?= Html::submitButton('Зарегистрироваться', ['class' => 'btn btn-success']) ?></div>
+			<div class="rounded my-3 p-3 d-flex justify-content-center" style="background-color:#2a2b38">
+
+				<?php $form = ActiveForm::begin(['options'=>['class'=>'w-100']]); ?>
+				<h1 class="text-center mb-4"><?= Html::encode($this->title) ?></h1>
+
+				<?= $form->field($model, 'login')->textInput(['autofocus' => true , 'class'=>'form-style ', 'placeholder'=>'введите логин'])->label(false) ?>
+				<?= $form->field($model, 'password')->passwordInput(['class'=>'form-style', 'placeholder'=>'введите пароль'])->label(false) ?>
+				<?= $form->field($model, 'name')->textInput([ 'class'=>'form-style', 'placeholder'=>'введите ваше имя'])->label(false) ?>
+				<div class="d-flex justify-content-center mt-4"><?= Html::submitButton('Зарегистрироваться', ['class' => 'btn btn-success']) ?></div>
 				<?php ActiveForm::end(); ?>
 			</div>
 		</div>
 	</div>
 </div>
+<style>
+	
+	
+</style>
